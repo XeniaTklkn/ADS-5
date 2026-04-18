af77c416aa742b9388dbeedf882d916299b07a68
XeniaTklkn <tukalkinaksenia@gmail.com>
2026-04-18 20:39:08 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
-#ifndef TSTACK_H
-#define TSTACK_H
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
 template<typename T, int kSize>
 class TStack {
@@ -36,4 +36,4 @@ class TStack {
   }
 };
 
-#endif  // TSTACK_H
+#endif  // INCLUDE_TSTACK_H_
